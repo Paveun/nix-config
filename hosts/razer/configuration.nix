3aa95fc4351a1ef4ab1f5a40c2428646c02e8e64
Paveun <a.tresierrajansen@gmail.com>
2026-02-12 03:10:23 -0500

    add gemini-cli

--- hosts/razer/configuration.nix
+++ hosts/razer/configuration.nix
@@ -65,8 +65,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -131,6 +131,7 @@
     gh
     nil
     nixd
+    gemini-cli
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
