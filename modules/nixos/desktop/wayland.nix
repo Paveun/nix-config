464828c4ceff9b9bc518f1386500caa6963144ed
Paveun <a.tresierrajansen@gmail.com>
2026-02-18 12:37:56 -0500

    add styling and settings to wofi

--- modules/nixos/desktop/wayland.nix
+++ modules/nixos/desktop/wayland.nix
@@ -22,6 +22,7 @@
         noto-fonts
         nerd-fonts.dejavu-sans-mono
         nerd-fonts.ubuntu-mono
+        nerd-fonts.cascadia-mono
       ];
 
       environment.sessionVariables.NIXOS_OZONE_WL = "1";
