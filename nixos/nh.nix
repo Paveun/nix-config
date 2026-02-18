4b260c4ca9a8ddd0ec02b7eef6ceda1830a5cc7e
Paveun <a.tresierrajansen@gmail.com>
2026-02-18 00:19:26 -0500

    Remove legacy nh NixOS configuration

--- nixos/nh.nix
+++
@@ -1,8 +0,0 @@
-{
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 8d --keep 20";
-    flake = "/home/paveun/nix-config"; # sets NH_OS_FLAKE variable for you
-  };
-}
