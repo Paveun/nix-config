238142889a8af8f97097b7d0e555f53a6edd1910
Paveun <a.tresierrajansen@gmail.com>
2026-02-21 04:13:26 -0500

    add plymouth config

--- modules/nixos/core/bootloader.nix
+++ modules/nixos/core/bootloader.nix
@@ -1,16 +1,30 @@
 {
   flake.modules.nixos.bootloader =
-    { lib, ... }:
+    { lib, pkgs, ... }:
     {
-      boot.loader = {
-        limine = {
-          enable = true;
-          maxGenerations = 15;
+      boot = {
+        loader = {
+          limine = {
+            enable = true;
+            maxGenerations = 15;
+          };
+
+          efi.canTouchEfiVariables = true;
+
+          timeout = lib.mkDefault 10;
         };
-
-        efi.canTouchEfiVariables = true;
-
-        timeout = lib.mkDefault 10;
+        consoleLogLevel = 3;
+        initrd.verbose = false;
+        initrd.systemd.enable = true;
+        kernelParams = [
+          "quiet"
+          "udev.log_level=3"
+          "systemd.show_status=auto"
+        ];
+        plymouth = {
+          enable = true;
+          font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNerdFont-Regular.ttf";
+        };
       };
     };
 }
