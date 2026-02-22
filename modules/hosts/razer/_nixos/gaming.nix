e842c4dd1f7667f3a3cb89b0c216613e9e69738f
Paveun <a.tresierrajansen@gmail.com>
2026-02-22 04:22:36 -0500

    Enable gamescope and add Proton GE package

--- modules/hosts/razer/_nixos/gaming.nix
+++ modules/hosts/razer/_nixos/gaming.nix
@@ -1,14 +1,29 @@
+{ pkgs, ... }:
 {
   # Enable the xone driver for Xbox controllers
   hardware.xone.enable = true;
 
-  programs.steam = {
-    enable = true;
+  programs = {
+    steam = {
+      enable = true;
 
-    # Open ports in the firewall for Steam Remote Play
-    remotePlay.openFirewall = true;
+      # Open ports in the firewall for Steam Remote Play
+      remotePlay.openFirewall = true;
 
-    # Open ports in the firewall for Source Dedicated Server
-    dedicatedServer.openFirewall = true;
+      # Open ports in the firewall for Source Dedicated Server
+      dedicatedServer.openFirewall = true;
+
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+    };
+
+    gamescope = {
+      enable = true;
+      capSysNice = false;
+    };
   };
+  environment.systemPackages = with pkgs; [
+    gamescope-wsi
+  ];
 }
