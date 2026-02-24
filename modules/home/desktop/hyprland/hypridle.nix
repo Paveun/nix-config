1e791b4a8e079d71b8b540c9eee4131fe99ce11d
Paveun <a.tresierrajansen@gmail.com>
2026-02-24 16:57:00 -0500

    Resolve hyprctl, brightnessctl and hyprlock via Nix and add a screen dim effect before lock

--- modules/home/desktop/hyprland/hypridle.nix
+++ modules/home/desktop/hyprland/hypridle.nix
@@ -1,25 +1,44 @@
 {
-  flake.modules.homeManager.hyprland = {
-    services.hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          lock_cmd = "pidof hyprlock || hyprlock";
-          before_sleep_cmd = "loginctl lock-session";
-          after_sleep_cmd = "hyprctl dispatch dpms on";
+  flake.modules.homeManager.hyprland =
+    {
+      lib,
+      pkgs,
+      config,
+      ...
+    }:
+    let
+      inherit (lib) getExe getExe';
+      brightnessctl = getExe pkgs.brightnessctl;
+      hyprctl = getExe' config.wayland.windowManager.hyprland.package "hyprctl";
+      hyprlock = getExe config.programs.hyprlock.package;
+    in
+    {
+      services.hypridle = {
+        enable = true;
+        settings = {
+          general = {
+            lock_cmd = "pidof ${hyprlock} || ${hyprlock}";
+            before_sleep_cmd = "loginctl lock-session";
+            after_sleep_cmd = "${hyprctl} dispatch dpms on";
+          };
+          listener = [
+            {
+              # dim screen before lock
+              timeout = 240;
+              on-timeout = "${brightnessctl} -s set 80%-";
+              on-resume = "${hyprctl} dispatch dpms on && ${brightnessctl} -r";
+            }
+            {
+              timeout = 300;
+              on-timeout = "loginctl lock-session";
+            }
+            {
+              timeout = 330;
+              on-timeout = "${hyprctl} dispatch dpms off && ${brightnessctl} -sd '*::kbd_backlight' set 0";
+              on-resume = "${hyprctl} dispatch dpms on && ${brightnessctl} -rd '*::kbd_backlight'";
+            }
+          ];
         };
-        listener = [
-          {
-            timeout = 300;
-            on-timeout = "loginctl lock-session";
-          }
-          {
-            timeout = 330;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
-          }
-        ];
       };
     };
-  };
 }
