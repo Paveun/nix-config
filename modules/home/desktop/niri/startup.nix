f0674114704d76f608172ed1bda2acd328811f2b
Paveun <a.tresierrajansen@gmail.com>
2026-02-26 13:30:23 -0500

    Resolve startup executables and add kanshi

--- modules/home/desktop/niri/startup.nix
+++ modules/home/desktop/niri/startup.nix
@@ -1,10 +1,17 @@
 {
-  flake.modules.homeManager.niri = {
-    programs.niri.settings = {
-      # ...
-      spawn-at-startup = [
-        { argv = [ "noctalia-shell" ]; }
-      ];
+  flake.modules.homeManager.niri =
+    {
+      lib,
+      pkgs,
+      config,
+      ...
+    }:
+    {
+      programs.niri.settings = {
+        spawn-at-startup = [
+          { argv = [ "${lib.getExe config.programs.noctalia-shell.package}" ]; }
+          { argv = [ "${lib.getExe pkgs.kanshi}" ]; }
+        ];
+      };
     };
-  };
 }
