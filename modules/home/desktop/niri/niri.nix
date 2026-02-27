48aa723ca661f070c2c5a34082fab11dfa68ec48
Paveun <a.tresierrajansen@gmail.com>
2026-02-27 11:29:55 -0500

    Configure Wayland and Qt theme for Niri

--- modules/home/desktop/niri/niri.nix
+++ modules/home/desktop/niri/niri.nix
@@ -13,16 +13,19 @@
         MOZ_ENABLE_WAYLAND = "1";
         ELECTRON_OZONE_PLATFORM_HINT = "wayland";
         OZONE_PLATFORM = "wayland";
+        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+        XDG_SESSION_TYPE = "wayland";
+        XDG_CURRENT_DESKTOP = "niri";
 
         QT_QPA_PLATFORM = "wayland";
-        QT_STYLE_OVERRIDE = "kvantum";
+        # QT_STYLE_OVERRIDE = "kvantum";
         SDL_VIDEODRIVER = "wayland";
 
-        XCOMPOSEFILE = "~/.XCompose";
+        # XCOMPOSEFILE = "~/.XCompose";
         EDITOR = "nvim";
 
         # Qt theming
-        QT_QPA_PLATFORMTHEME = "qt6ct";
+        QT_QPA_PLATFORMTHEME = "gtk3";
       };
 
       debug = {
