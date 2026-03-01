e73cac9abc61cdb302a170aebb75b717aa9e896b
Paveun <a.tresierrajansen@gmail.com>
2026-03-01 01:24:19 -0500

    Add LibrePods desktop entry and package variable

--- modules/home/desktop/librepods.nix
+++ modules/home/desktop/librepods.nix
@@ -5,9 +5,23 @@
       pkgs,
       ...
     }:
+    let
+      librepods = inputs.librepods.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    in
     {
-      home.packages = [
-        inputs.librepods.packages.${pkgs.stdenv.hostPlatform.system}.default
-      ];
+      home.packages = [ librepods ];
+
+      xdg.desktopEntries.librepods = {
+        name = "LibrePods";
+        comment = "AirPods liberated from Apple's ecosystem";
+        exec = "${librepods}/bin/librepods";
+        icon = "${inputs.librepods}/linux-rust/assets/icon.png";
+        terminal = false;
+        type = "Application";
+        categories = [
+          "Utility"
+          "Audio"
+        ];
+      };
     };
 }
