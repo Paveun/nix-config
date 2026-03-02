a576c22a7c0cb4418268ab0c78590989afdfd964
Paveun <a.tresierrajansen@gmail.com>
2026-03-02 01:54:01 -0500

    Comment out NIXOS_OZONE_WL session variable

--- modules/hosts/razer/_home/default.nix
+++ modules/hosts/razer/_home/default.nix
@@ -2,7 +2,7 @@
 {
   # Unset NIXOS_OZONE_WL fixes flickering Discord on sway+Nvidia
   # https://discourse.nixos.org/t/nvidia-sway-flickering/65262/18
-  home.sessionVariables.NIXOS_OZONE_WL = lib.mkForce 0;
+  # home.sessionVariables.NIXOS_OZONE_WL = lib.mkForce 0;
   # wayland.windowManager.sway.extraOptions = [ "--unsupported-gpu" ];
 
 }
