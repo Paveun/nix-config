939399b7b8c37ff0236a5c5555d6a4513161c9d3
Paveun <a.tresierrajansen@gmail.com>
2026-03-03 17:18:24 -0500

    Add desktop profile and enable Alienware screen

--- modules/hosts/aion/_home/kanshi.nix
+++ modules/hosts/aion/_home/kanshi.nix
@@ -4,7 +4,11 @@
       criteria = "DP-1";
       mode = "3440x1440@174.963Hz";
       position = "0,0";
-      scale = 1;
+      scale = 1.0;
+    };
+    _profiles.desktop = {
+      wifi = false;
+      screens.alienware.status = "enable";
     };
   };
 }
