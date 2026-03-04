270fa1aea5e1e748285b882f6bb9f7568d9defae
Paveun <a.tresierrajansen@gmail.com>
2026-03-04 01:00:06 -0500

    adjust kanshi configuration

--- modules/hosts/razer/_home/kanshi.nix
+++ modules/hosts/razer/_home/kanshi.nix
@@ -4,7 +4,11 @@
       criteria = "eDP-1";
       mode = "1920x1080@144.00Hz";
       position = "0,0";
-      scale = 1;
+      scale = 1.0;
+    };
+    _profiles.desktop = {
+      wifi = true;
+      screens.laptop.status = "enable";
     };
   };
 }
