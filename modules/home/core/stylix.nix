a4b94dfedeeba8b0736f6277ee9b70a5a1b455e3
Paveun <a.tresierrajansen@gmail.com>
2026-03-04 23:39:59 -0500

    Add Stylix wallpaper image

--- modules/home/core/stylix.nix
+++ modules/home/core/stylix.nix
@@ -7,6 +7,7 @@
         enable = true;
         base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
         polarity = "dark";
+        image = ../desktop/wallpaper.png;
 
         fonts = {
           sizes = {
