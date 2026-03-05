8ae364956f6ded65e6101537e8a14e9c0a5c2533
Paveun <a.tresierrajansen@gmail.com>
2026-03-04 23:10:52 -0500

    Use precise refresh rate for laptop screen

--- modules/hosts/razer/_home/kanshi.nix
+++ modules/hosts/razer/_home/kanshi.nix
@@ -2,7 +2,7 @@
   services.kanshi = {
     screens.laptop = {
       criteria = "eDP-1";
-      mode = "1920x1080@144.00Hz";
+      mode = "1920x1080@143.998Hz";
       position = "0,0";
       scale = 1.0;
     };
