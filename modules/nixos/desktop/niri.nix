274be905c7dfbcee44d23de1959c4800beac0a30
Paveun <a.tresierrajansen@gmail.com>
2026-03-05 11:36:02 -0500

    Configure xdg portals for niri

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -20,5 +20,16 @@
         upower.enable = true;
       };
 
+      xdg.portal.config.niri = {
+        default = [
+          "gnome"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Access" = [ "gtk" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+      };
+
     };
 }
