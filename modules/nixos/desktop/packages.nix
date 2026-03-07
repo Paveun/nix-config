4f80512c476ce3a4f13fa71cee7390cb216beea6
Paveun <70171203+Paveun@users.noreply.github.com>
2026-03-07 02:08:53 -0500

    package rework (#12)
    * switch to element-desktop for matrix
    
    * add also-scarlette-gui for aion
    
    * Fix typo in scarlette import

--- modules/nixos/desktop/packages.nix
+++ modules/nixos/desktop/packages.nix
@@ -3,7 +3,7 @@
     { pkgs, ... }:
     {
       # Needed by the nheko matrix client
-      nixpkgs.config.permittedInsecurePackages = [ "olm-3.2.16" ];
+      # nixpkgs.config.permittedInsecurePackages = [ "olm-3.2.16" ];
 
       environment.systemPackages = with pkgs; [
 
