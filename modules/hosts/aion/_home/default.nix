dc19e987b757a1a8fe6a351a35184f0c1c5b81a2
Paveun <a.tresierrajansen@gmail.com>
2026-03-07 01:52:45 -0500

    add also-scarlette-gui for aion

--- modules/hosts/aion/_home/default.nix
+++ modules/hosts/aion/_home/default.nix
@@ -3,6 +3,7 @@
   imports = [
     ./kanshi.nix
     ./gtk.nix
+    ./scarlet.nix
   ];
   # Unset NIXOS_OZONE_WL fixes flickering Discord on sway+Nvidia
   # https://discourse.nixos.org/t/nvidia-sway-flickering/65262/18
