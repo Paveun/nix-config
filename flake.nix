ade68981a95936c866b1f7a509223f3f2f7a8d36
Paveun <a.tresierrajansen@gmail.com>
2026-03-11 03:28:23 -0400

    Remove trailing slash from home-manager URL in flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
